3a6a634bfad0169be9fb9994db79c1fd2fd87bd4
Chano Vera <github@chanovera.com>
2025-10-25 13:43:54 -0600

    fix aria-label

--- templates/frontpage/testimonies.php
+++ templates/frontpage/testimonies.php
@@ -51,7 +51,7 @@
                                 <figure class="testimonial-media" aria-hidden="true">
                                     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/alejandro-ruiz.webp" alt="Foto de Alejandro Ruiz" class="testimonial-avatar">
                                 </figure>
-                                <p id="t2-name" class="testimonial-author">Alejandro Ruiz</p>
+                                <p class="testimonial-author">Alejandro Ruiz</p>
                                 <span class="testimonial-role">Empresario, Guadalajara</span>
                             </div>
                             <blockquote class="testimonial-quote" cite="">
@@ -77,7 +77,7 @@
                                 <figure class="testimonial-media" aria-hidden="true">
                                     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/mariela-roberto-torres.webp" alt="Foto de Mariela y Roberto Torres" class="testimonial-avatar">
                                 </figure>
-                                <p id="t3-name" class="testimonial-author">Mariela y Roberto Torres</p>
+                                <p class="testimonial-author">Mariela y Roberto Torres</p>
                                 <span class="testimonial-role">Padres de familia, Puebla</span>
                             </div>
                             <blockquote class="testimonial-quote" cite="">
@@ -103,7 +103,7 @@
                                 <figure class="testimonial-media" aria-hidden="true">
                                     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gabriela-lozano.webp" alt="Foto de Gabriela Lozano" class="testimonial-avatar">
                                 </figure>
-                                <p id="t4-name" class="testimonial-author">Gabriela Lozano</p>
+                                <p class="testimonial-author">Gabriela Lozano</p>
                                 <span class="testimonial-role">Inversionista, CDMX</span>
                             </div>
                             <blockquote class="testimonial-quote" cite="">
@@ -129,7 +129,7 @@
                                 <figure class="testimonial-media" aria-hidden="true">
                                     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/carlos-mendez.webp" alt="Foto de Carlos Méndez" class="testimonial-avatar">
                                 </figure>
-                                <p id="t5-name" class="testimonial-author">Carlos Méndez</p>
+                                <p class="testimonial-author">Carlos Méndez</p>
                                 <span class="testimonial-role">Ingeniero, Querétaro</span>
                             </div>
                             <blockquote class="testimonial-quote" cite="">
@@ -155,7 +155,7 @@
                                 <figure class="testimonial-media" aria-hidden="true">
                                     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/paola-sanchez.webp" alt="Foto de Paola Sánchez" class="testimonial-avatar">
                                 </figure>
-                                <p id="t6-name" class="testimonial-author">Paola Sánchez</p>
+                                <p class="testimonial-author">Paola Sánchez</p>
                                 <span class="testimonial-role">Emprendedora, Mérida</span>
                             </div>
                             <blockquote class="testimonial-quote" cite="">
@@ -166,9 +166,9 @@
                 </li>
             </ul>
             <div class="slideshow-bullets-wrapper">
-                <button class="slideshow-prev btn-pagination glass-bright"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/></svg></button>
+                <button class="slideshow-prev btn-pagination glass-bright" aria-label="Paginación para carrusel"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/></svg></button>
                 <ul class="slideshow-bullets"></ul>
-                <button class="slideshow-next btn-pagination glass-bright"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/></svg></button>
+                <button class="slideshow-next btn-pagination glass-bright" aria-label="Paginación para carrusel"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/></svg></button>
             </div>
         </div>
     </div>
