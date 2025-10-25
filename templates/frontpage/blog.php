562a2c138b552b0936822a2e4a2211cc754f8050
Chano Vera <github@chanovera.com>
2025-10-25 13:50:05 -0600

    fix aria-label

--- templates/frontpage/blog.php
+++ templates/frontpage/blog.php
@@ -2,6 +2,6 @@
     <div class="content">
         <h2 class="title-section">BLOG</h2>
         <div class="posts"></div>
-        <button onclick="window.location.href='<?php echo site_url(); ?>/blog'" class="go-to-blog" aria-name="Link to go blog page">Ver todos</button>
+        <button onclick="window.location.href='<?php echo site_url(); ?>/blog'" class="go-to-blog" aria-label="Link to go blog page">Ver todos</button>
     </div>
 </section>
